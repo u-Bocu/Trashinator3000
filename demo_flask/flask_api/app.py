0e33dbc833f43c2434a78f9ff7b69d8ffb8ff17a
Jonathan PEREIRA <j.pereira@autom-elec.com>
2022-11-28 18:18:34 +0100

    Récupération images + print --> Ok

--- demo_flask/flask_api/app.py
+++ demo_flask/flask_api/app.py
@@ -1,25 +1,25 @@
 from flask import render_template, request
+from model import get_trash
 import os
 
 import connexion
-from model import *
+
 
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("../openapi/swagger.yml")
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['POST'])
 def result():
     if request.method == 'POST':
         f = request.files['image']
-        f.save(os.path.join('./Images', f.filename))
-        #get_trash(f.filename)
-    return render_template("home.html")
-        
+        f.save(os.path.join('./Images/', f.filename))
 
+        conf, data = get_trash(f.filename)
+    return render_template("home.html", data=data, conf=conf)
+        
 @app.route("/")
 def home():
     return render_template("home.html")
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
