4b062192fae00b222a2430e003f53764ea1fadac
eirla42 <math.poke@gmail.com>
2022-11-29 08:40:29 +0100

    Connexion entre Flask Api et Angular

--- demo_flask/flask_api/app.py
+++ demo_flask/flask_api/app.py
@@ -1,12 +1,11 @@
-from flask import render_template, request
+from flask import Flask, render_template, request
+from flask_cors import CORS
 from model import get_trash
 import os
 
-import connexion
+app = Flask(__name__)
+CORS(app)
 
-
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("../openapi/swagger.yml")
 
 @app.route("/", methods=['POST'])
 def result():
@@ -16,10 +15,33 @@ def result():
 
         conf, data = get_trash(f.filename)
     return render_template("home.html", data=data, conf=conf)
-        
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+PEOPLE = {
+    "Fairy": {
+        "fname": "Tooth",
+        "lname": "Fairy"
+    },
+    "Ruprecht": {
+        "fname": "Knecht",
+        "lname": "Ruprecht"
+    },
+    "Bunny": {
+        "fname": "Easter",
+        "lname": "Bunny"
+    }
+}
+
+
+@app.route("/api/people", methods=['GET'])
+def read_all():
+    return list(PEOPLE.values())
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
