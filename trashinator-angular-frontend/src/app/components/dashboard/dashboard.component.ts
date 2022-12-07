8c2c5b1bb37d3c704099ad087111829c5dc63f79
Mathieu Maisonnette <math.poke@gmail.com>
2022-12-07 18:36:52 +0100

    Echart + custom scrollbar

--- trashinator-angular-frontend/src/app/components/dashboard/dashboard.component.ts
+++ trashinator-angular-frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,33 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
+export class DashboardComponent implements OnInit {
+  nbColonnes?: number = 4;
+  cards?: Observable<Array<any>>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    /* Si l'écran est petit, passes les 'cards' de la taille standard vers une colonne */
+    this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+      map(({ matches }) => {
+        if (matches) {
+          // Taille sur colonne
+          this.nbColonnes = 1;
+          return [
+            { title: 'Card 1', cols: 1, rows: 1 },
+            { title: 'Card 2', cols: 1, rows: 1 },
+            { title: 'Card 3', cols: 1, rows: 1 },
+            { title: 'Card 4', cols: 1, rows: 1 },
+            { title: '', cols: 1, rows: 2, content: 'nbScansChartComponent' }
+          ];
+        }
+
+        // Taille standard
+        this.nbColonnes = 4;
         return [
           { title: 'Card 1', cols: 1, rows: 1 },
           { title: 'Card 2', cols: 1, rows: 1 },
           { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Card 4', cols: 1, rows: 1 },
+          { title: '',cols: 4, rows: 2, content: 'nbScansChartComponent' }
         ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+      })
+    );
+  }
 }
