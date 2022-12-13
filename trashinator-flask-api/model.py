066e007ccf05db38000eca2a72d062033748d1a2
Mathieu Maisonnette <math.poke@gmail.com>
2022-12-13 20:02:52 +0100

    Refactor

--- trashinator-flask-api/model.py
+++ trashinator-flask-api/model.py
@@ -11,12 +11,14 @@ labels = ['G&M', 'Organic', 'Other', 'Paper', 'Plastic']
 le = LabelEncoder()
 labels = le.fit_transform(labels)
 
+
 # Resize the image for preprocessing.
 def pre(img_to_resize):
     img_pre = img_to_resize.resize((224, 224))
     img_pre = img_to_array(img_pre)
     img_pre = np.expand_dims(img_pre / 255, 0)
     return img_pre
+
 
 # The model predicts an output depending on the image.
 # Returns the confidence and the predicted class.
@@ -25,14 +27,9 @@ def get_trash(img):
     p = model.predict(img)
 
     confidences = max(np.squeeze(model.predict(img)))
-    conf = round(confidences * 100, 1) 
+    conf = round(confidences * 100, 1)
 
     predicted_class = le.classes_[np.argmax(p[0], axis=-1)]
     print(predicted_class)
 
     return conf, predicted_class
-    
-'''
-if __name__=='__main__':
-    get_trash()
-'''
