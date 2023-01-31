4fa597669bf62d07b567b3fc3067d8d83cfe81f7
Mathieu Maisonnette <math.poke@gmail.com>
2023-01-31 17:15:18 +0100

    Config File

--- trashinator-angular-frontend/src/app/services/world-bank.service.ts
+++ trashinator-angular-frontend/src/app/services/world-bank.service.ts
@@ -1,30 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { HttpClient } from "@angular/common/http"
 import { Observable } from "rxjs";
+import { environment } from "../environnement";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorldBankService {
 
-  rootURL = 'http://localhost:8080/api';
-  headers = new HttpHeaders()
-    .set('Content-Type', 'application/json')
-    .set('Access-Control-Allow-Origin', '*');
-
   constructor(private http: HttpClient) { }
 
   public getWorldStatistics(country_name: string): Observable<any> {
     return this.http.get(
-      this.rootURL + '/global_data/world_data?countryName=' + country_name,
-      { headers: this.headers }
+      environment.API_URL + '/global_data/world_data?countryName=' + country_name,
+      { headers: environment.HEADERS }
     )
   }
 
   public getListOfCountry(): Observable<any> {
     return this.http.get(
-      this.rootURL + '/global_data/country_name',
-      { headers: this.headers }
+      environment.API_URL + '/global_data/country_name',
+      { headers: environment.HEADERS }
     )
   }
 }
